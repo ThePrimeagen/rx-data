7ccc17c252f500d6e0de2a4b54fd444258b8967a
brentechols34 <brentechols34@yahoo.com>
2013-10-14 21:15:38 -0600

    Added method signatures to the rx-data object

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var matrix= require('./lib/matrix');
 var RxData = {
     correlation: correlation,
     transform: matrix.transform,
-    multiply: matrix.multiply
-};
+    multiply: matrix.multiply,
+    add: matrix.add,
+    subtract: matrix.subtract
+}
 
 module.exports = RxData;
