c54a24cbbb448695bd41c7b1b9830749f39aec12
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-14 20:44:15 -0600

    Fixed up some stuff.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var correlation = require('./lib/correlation');
 var matrix= require('./lib/matrix');
 var RxData = {
     correlation: correlation,
-    transform: matrix.transform
+    transform: matrix.transform,
+    multiply: matrix.multiply
 };
 
-module.exports = RxStats;
+module.exports = RxData;
